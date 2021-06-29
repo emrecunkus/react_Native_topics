abe9932b3449c495f71dd8d6e462ff27b727e9e3
emre2023 <muhammed8336@gmail.com>
2021-06-29 17:13:34 +0300

    changes 29.06.2021

--- topics/Buttons_.js
+++ topics/Buttons_.js
@@ -1,24 +1,70 @@
 import React, { Component } from 'react';  
-import { AppRegistry, Text, TextInput, View } from 'react-native';  
+import { Alert,Platform,StyleSheet,Text,TouchableHighlight,TouchableOpacity,  
+    TouchableNativeFeedback,TouchableWithoutFeedback, View } from 'react-native';  
   
-export default class PizzaTranslator extends Component {  
-    constructor(props) {  
-        super(props);  
-        this.state = {text: ''};  
+export default class Buttons_ extends Component {  
+
+  // here are the functions 
+    _onPressButton() {  
+        Alert.alert('You tapped the button!')  
     }  
+  
+    _onLongPressButton() {  
+        Alert.alert('You long-pressed the button!')  
+    }  
+  
   
     render() {  
         return (  
-            <View style={{padding: 10}}>  
-                <TextInput  
-                    style={{height: 40,backgroundColor: 'azure', fontSize: 20}}  
-                    placeholder="Type here to translate!"  
-                    onChangeText={(text) => this.setState({text})}  
-                />  
-                <Text style={{padding: 100, fontSize: 50}}>  
-                    {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}  
-                </Text>*  
+            <View style={styles.container}>  
+                <TouchableHighlight onPress={this._onPressButton} underlayColor="white">  
+                    <View style={styles.button}>  
+                        <Text style={styles.buttonText}>TouchableHighlight</Text>  
+                    </View>  
+                </TouchableHighlight>  
+                <TouchableOpacity onPress={this._onPressButton}>  
+                    <View style={styles.button}>  
+                        <Text style={styles.buttonText}>TouchableOpacity</Text>  
+                    </View>  
+                </TouchableOpacity>  
+                <TouchableNativeFeedback  
+                    onPress={this._onPressButton}  
+                    background={Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : ''}>  
+                    <View style={styles.button}>  
+                        <Text style={styles.buttonText}>TouchableNativeFeedback</Text>  
+                    </View>  
+                </TouchableNativeFeedback>  
+                <TouchableWithoutFeedback  
+                    onPress={this._onPressButton}  
+                >  
+                    <View style={styles.button}>  
+                        <Text style={styles.buttonText}>TouchableWithoutFeedback</Text>  
+                    </View>  
+                </TouchableWithoutFeedback>  
+                <TouchableHighlight onPress={this._onPressButton} onLongPress={this._onLongPressButton} underlayColor="white">  
+                    <View style={styles.button}>  
+                        <Text style={styles.buttonText}>Touchable with Long Press</Text>  
+                    </View>  
+                </TouchableHighlight>  
             </View>  
         );  
     }  
 }  
+  
+const styles = StyleSheet.create({  
+    container: {  
+        paddingTop: 60,  
+        alignItems: 'center'  
+    },  
+    button: {  
+        marginBottom: 30,  
+        width: 260,  
+        alignItems: 'center',  
+        backgroundColor: 'red'  
+    },  
+    buttonText: {  
+        padding: 20,  
+        color: 'black',  
+        fontSize: 18  
+    }  
+});  
